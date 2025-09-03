d825167f23936dbc3a6e0a89334580ac48169d75
nickerick <nickerickson737@gmail.com>
2025-09-03 17:39:43 -0500

    cleanup testclient

--- apps/server/src/tool/test-client.ts
+++ apps/server/src/tool/test-client.ts
@@ -4,7 +4,7 @@ import {
     Message,
     Serializer,
 } from "telepact";
-import { ClientInterface_, ping2 } from "./gen/all_";
+import { ClientInterface_, exampleFunction2 } from "../gen/all_";
 
 const adapter: (m: Message, s: Serializer) => Promise<Message> = async (m, s) => {
     // Debug
@@ -45,7 +45,7 @@ const client = new Client(adapter, options);
 const genClient = new ClientInterface_(client);
 
 // Make telepact request
-const resp = await genClient.ping2({}, ping2.Input.fromTyped({}));
+const resp = await genClient.exampleFunction2({}, exampleFunction2.Input.fromTyped({field: 3}));
 
 // Debug
 console.log('Telepact Response');
