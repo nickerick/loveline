976e4517c70c18268b6b6e2e613ef3e9911ec31a
nickerick <nickerickson737@gmail.com>
2025-09-08 21:15:47 -0500

    fix gen import

--- apps/server/src/handler.ts
+++ apps/server/src/handler.ts
@@ -1,11 +1,11 @@
-import { ServerHandler_, ping2 } from './gen/all_.js';
+import { ServerHandler_, exampleFunction2 } from './gen/all_.js';
 
 export class ServerHandler extends ServerHandler_ {
 
-    override async ping2(headers: Record<string, any>, input: ping2.Input): Promise<[Record<string, any>, ping2.Output]> {
+    override async exampleFunction2(headers: Record<string, any>, input: exampleFunction2.Input): Promise<[Record<string, any>, exampleFunction2.Output]> {
         console.log('here1');
 
-        let output = ping2.Output.from_Ok_(ping2.Output.Ok_.fromTyped({}));
+        let output = exampleFunction2.Output.from_Ok_(exampleFunction2.Output.Ok_.fromTyped({}));
         return [{}, output];
     }
 }
