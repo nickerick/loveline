4b9a5210ec19517e6d10ee680706e111dbd70dc9
nickerick <nickerickson737@gmail.com>
2025-09-10 17:38:00 -0500

    install prettier for mobile

--- apps/mobile/src/domain/user/User.ts
+++ apps/mobile/src/domain/user/User.ts
@@ -6,11 +6,16 @@ export class User {
     public username: string,
     public email: string,
     public firstName: string,
-    public lastName: string
-  ) { }
+    public lastName: string,
+  ) {}
 
   static fromTelepact(user: User_): User {
-
-    return new User(user.id(), user.username(), user.email(), user.firstName(), user.lastName());
+    return new User(
+      user.id(),
+      user.username(),
+      user.email(),
+      user.firstName(),
+      user.lastName(),
+    );
   }
 }
--- apps/mobile/src/domain/user/UserService.ts
+++ apps/mobile/src/domain/user/UserService.ts
@@ -1,20 +1,23 @@
-import { exampleFunction2, getUsers, TelepactClient } from "../../infrastructure/TelepactService";
+import {
+  exampleFunction2,
+  getUsers,
+  TelepactClient,
+} from "../../infrastructure/TelepactService";
 import { User } from "./User";
 
 export class UserService {
-  constructor(private client: TelepactClient) { }
+  constructor(private client: TelepactClient) {}
 
   async getAllUsers(): Promise<User[]> {
-      const resp = await this.client.getUsers(
-        {},
-        getUsers.Input.fromTyped({})
-      );
+    const resp = await this.client.getUsers({}, getUsers.Input.fromTyped({}));
 
-      if (resp[1].getTaggedValue().tag === "Ok_") {
-        const apiUsers = (resp[1].getTaggedValue().value as getUsers.Output.Ok_).users();
-        return apiUsers.map(User.fromTelepact);
-      }
-      
+    if (resp[1].getTaggedValue().tag === "Ok_") {
+      const apiUsers = (
+        resp[1].getTaggedValue().value as getUsers.Output.Ok_
+      ).users();
+      return apiUsers.map(User.fromTelepact);
+    }
+
     return [];
   }
 }
